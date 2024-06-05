6ecc34857d9658af0bf75ff2e5275427f97780a2
Josh Beaver <josh.elvn11@gmail.com>
2024-06-05 15:39:54 +0100

    [MISC] created accommodation serializer

---
+++ accommodation/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import AccommodationListing
+
+
+class AccommodationListingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AccommodationListing
+        fields = [
+            'id', 'site', 'accommodation_name', 'accommodation_slug',
+            'accommodation_type', 'accommodation_price_type',
+            'accomodation_price', 'accommodation_description',
+            'accommodation_excerpt'
+        ]
